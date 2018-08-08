213fa400ea714cccfd7d73b97f6421c042c16191
Rudy Harrelson <rudyharrelson@gmail.com>
2018-08-08 04:14:03 -0400

    housekeeping

--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
-python -B -O -m PyInstaller --onefile --icon=./media/app.ico --distpath ./bin --specpath ./temp --workpath ./temp ./src/swearjar.py
+python -O -m PyInstaller --onefile --icon=./media/app.ico --distpath ./bin --specpath ./temp --workpath ./temp ./src/swearjar.py
 
 echo y|rmdir /s temp
+
+echo y|rmdir /s src\__pycache__
